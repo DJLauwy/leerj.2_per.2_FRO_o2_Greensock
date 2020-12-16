e1c405577961811dab2d45d7353fa38f9aa84fe7
DJLauwy <laudehoop@hotmail.com>
2020-12-16 11:44:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# leerj.2_per.2_FRO_o2_Greensock
+ Leerjaar 2 Periode 2 FRO opdracht 2 Greensock
---
+++ script.js
@@ -0,0 +1,16 @@
+//Voor deze opdracht wil ik graag met GreenSock werken.
+//Om van het animeren en interactie te kunnen leren, wil ik de volgende
+//elementen toevoegen:
+// - Een auto die een rondje rijdt
+// - Een klikbare ambulance met geluid en licht effect
+// - Een object waar je over kan hoveren en er verschijnt een animatie
+
+
+//GSAP mogelijkheden:
+// gsap.to('.auto', {duration: 2, top: '170px', left: '380px', ease: 'power'});
+// gsap.to('.auto', {duration: 2, rotation: '70deg', top: '180px', left: '405px', ease: 'power', delay: 1.8});
+
+//TweenLite mogelijkheden:
+//Auto
+TweenLite.to('.auto', 2, {top: '170px', left: '380px', ease: 'power'});
+TweenLite.to('.auto', 1, {rotation: '70deg', top: '180px', left: '405px', ease: 'power', delay: 1.8});
