d292223a8b0ea437074809d4fa624e41f94f8e25
DJLauwy <laudehoop@hotmail.com>
2020-12-16 15:22:57 +0100

    Extra auto toegeovegd

--- script.js
+++ script.js
@@ -19,16 +19,17 @@ let autoTween = function(){
 	auto_tl
 
 	.to('.auto', 2, {top: '170px', left: '380px'})
-	.to('.auto', .5, {rotation: '70deg', top: '180px', left: '405px'})
+	.to('.auto', 1, {rotation: '70deg', top: '180px', left: '405px'})
 	.to('.auto', 1, {top: '230px', left: '420px'})
 	.to('.auto', 1, {rotation: '90deg', top: '225px', left: '420px'})
 	.to('.auto', .5, {top: '248px', ease: 'sine'})
-	.to('.auto', 1, {top: '300px', ease: 'sine'})
-	.to('.auto', 1, {rotation: '65deg', top: '380px', left: '462px', ease: 'sine'})
-	.to('.auto', 1, {top: '480px', left: '495px', ease: 'sine'})
+	.to('.auto', 1, {top: '300px', ease: 'sine', delay: 1.5})
+	.to('.auto', .5, {rotation: '65deg'})
+	.to('.auto', 2, { top: '380px', left: '462px', ease: 'sine'})
+	.to('.auto', 2, {top: '480px', left: '495px', ease: 'sine'})
 	.to('.auto', .5, {rotation: '170deg', top: '485px', left: '500px', ease: 'sine'})
 	.to('.auto', 2, {top: '515px', left: '240px', ease: 'sine'})
-	.to('.auto', .5, {rotation: '250deg', top: '500px', left: '220px', ease: 'sine'})
+	.to('.auto', 1, {rotation: '250deg', top: '500px', left: '220px', ease: 'sine'})
 	.to('.auto', 1, {top: '410px', left: '190px', ease: 'sine'})
 	.to('.auto', .3, {rotation: '280deg', ease: 'sine'})
 	.to('.auto', 1, { top: '360px', left: '210px', ease: 'sine'})
@@ -39,4 +40,14 @@ let autoTween = function(){
 	//TweenLite.to('.auto', , {top: '', left: '', ease: 'sine', delay: });
 }
 
+//Wilde Auto
+let wildeAuto = function(){
+	let wilde = new TimelineMax({repeat: -1});
+	wilde
+	.to('.wilde', .5, {rotation: '120deg', top: '270px', left: '850px', ease: 'power0'})
+	.to('.wilde', .5, {rotation: '230deg', top: '350px', left: '800px', ease: 'power0'});
+}
+
+//uitvoeren functies
 autoTween();
+wildeAuto();
